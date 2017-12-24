757768f780872528a3e480a89d14b6c4c8b62751
flybyme <mangoraft@gmail.com>
2017-12-24 11:16:02 -0600

    code clean

--- README.md
+++ README.md
@@ -21,25 +21,29 @@ $ sudo npm install -g
 Starting the server is the first thing you want to do.
 The servers are setup to scale. You can run it as a cluster or run each part on different servers.
 ```
-$ logster-redis server -h
-
+$ nameserver server -h
 
   Usage: server [options]
 
-  Run the nameserver.
-
   Options:
 
-    -h, --help                   output usage information
-    -a, --addr [HOST]            Bind to HOST address (default: 127.0.0.1)
-    -p, --port [PORT]            Use PORT (default: 53)
-    -p, --port-udp [PORT-UDP]    Use PORT (default: 53)
-    -A, --redis-addr [HOST]      Connect to redis HOST address (default: 127.0.0.1)
-    -P, --redis-port [PORT]      Connect to redis PORT (default: 6379)
-    -o, --redis-auth [PASSWORD]  Use redis auth
-    -t, --tcp                    Start TCP-Server
-    -u, --udp                    Start UDP-Server
-    -c, --cluster                Start server as cluster
+    -h, --help                         output usage information
+    -a, --addr [HOST]                  Bind to HOST address (default: 127.0.0.1)
+    -p, --port [PORT]                  Use PORT (default: 53)
+    -p, --port-udp [PORT-UDP]          Use PORT (default: 53)
+    -A, --redis-addr [HOST]            Connect to redis HOST address (default: 127.0.0.1)
+    -P, --redis-port [PORT]            Connect to redis PORT (default: 6379)
+    -o, --redis-auth [PASSWORD]        Use redis auth
+    -t, --tcp                          Start TCP-Server
+    -u, --udp                          Start UDP-Server
+    -c, --cluster                      Start server as cluster
+    -l, --logging                      Start logging (default: false)
+    -v, --log-udp-port [LOG-UDP-PORT]  Use PORT (default: 5001)
+    -x, --log-tcp-port [LOG-TCP-PORT]  Use PORT (default: 5000)
+    -y, --log-host [HOST]              Use HOST (default: 127.0.0.1)
+    -z, --session [SESSION]            Use SESSION (default: dns)
+    -f, --channel [CHANNEL]            Use CHANNEL (default: ns.0)
+    -g, --source [SOURCE]              Use SOURCE (default: dns)
 
 ```
 #####Example
--- bin/nameserver.js
+++ bin/nameserver.js
@@ -3,19 +3,18 @@
 var program = require('commander');
 var cluster = require('cluster');
 var path = require('path');
-var ndns = require('native-dns');
+var ndns = require('native-dns-nameserver');
 var async = require('async');
 var tld = require('tldjs');
 var Logger = require('raft-logger-redis').Logger;
 
 program.version(require('../package.json').version);
-process.on('uncaughtException', function(err) {
-	console.log(err)
-});
 
 var server = program.command('server');
 server.description('Run the nameserver.');
 
+server.option('-i, --ipv6', 'use ipv6 (default: false)', false);
+server.option('-I, --ipv6-addr [HOST]', 'use ipv6 (default: ::)', "::");
 server.option('-a, --addr [HOST]', 'Bind to HOST address (default: 127.0.0.1)', '127.0.0.1');
 server.option('-p, --port [PORT]', 'Use PORT (default: 53)', 53);
 server.option('-p, --port-udp [PORT-UDP]', 'Use PORT (default: 53)', 53);
@@ -35,93 +34,105 @@ server.option('-f, --channel [CHANNEL]', 'Use CHANNEL (default: ns.0)', 'ns.0');
 server.option('-g, --source [SOURCE]', 'Use SOURCE (default: dns)', 'dns');
 server.action(function(options) {
 
-	var logHandler;
-	var redis = {
-		host : options.redisAddr,
-		port : options.redisPort
-	};
+    var logHandler;
+    var redis = {
+        host : options.redisAddr,
+        port : options.redisPort
+    };
 
-	if (options.redisAuth) {
-		redis.auth = options.redisAuth;
-	}
+    if (options.redisAuth) {
+        redis.auth = options.redisAuth;
+    }
 
-	if (options.logging) {
-		var logs = Logger.createLogger({
-			"web" : {
-				"port" : options.logTcpPort,
-				"host" : options.logHost
-			},
-			"udp" : {
-				"port" : options.logUdpPort,
-				"host" : options.logHost
-			},
-			"view" : {
-				"port" : options.logTcpPort,
-				"host" : options.logHost
-			}
-		});
+    if (options.logging) {
+        var logs = Logger.createLogger({
+            "web" : {
+                "port" : options.logTcpPort,
+                "host" : options.logHost
+            },
+            "udp" : {
+                "port" : options.logUdpPort,
+                "host" : options.logHost
+            },
+            "view" : {
+                "port" : options.logTcpPort,
+                "host" : options.logHost
+            }
+        });
 
-		logHandler = logs.create({
-			source : options.source,
-			channel : options.channel,
-			session : options.session,
-			bufferSize : 1
-		});
-	}
+        logHandler = logs.create({
+            source : options.source,
+            channel : options.channel,
+            session : options.session,
+            bufferSize : 1
+        });
+    }
 
-	var cache = require('../lib/cache')(redis, {
-		logHandler : logHandler
-	});
+    process.on('uncaughtException', function(err) {
+        (logHandler || console).log(err)
+    });
+    var cache = require('../lib/cache')(redis, {
+        logHandler : logHandler
+    });
 
-	var ns = require('../lib/ddns').create(cache);
+    var ns = require('../lib/ddns').create(cache);
 
-	if (options.cluster) {
-		var numCPUs = require('os').cpus().length;
-		if (cluster.isMaster) {
-			for (var i = 0; i < numCPUs; i++)
-				cluster.fork();
+    if (options.cluster) {
+        var numCPUs = require('os').cpus().length;
+        if (cluster.isMaster) {
+            for (var i = 0; i < numCPUs; i++)
+                cluster.fork();
 
-		} else {
-			tcp();
-			udp();
-		}
-	} else {
-		tcp();
-		udp();
-	}
+        } else {
+            tcp();
+            udp();
+        }
+    } else {
+        tcp();
+        udp();
+    }
 
-	function setupServer(server, type) {
-		server.on('error', function(err) {
-			(logHandler || console).log(err)
-		});
-		server.on('socketError', function(err) {
-			(logHandler || console).log(err)
-		});
-		server.on('request', ns);
-		server.on('listening', function() {
-			(logHandler || console).info(type + ' Server running: ', server.address().address + ':' + server.address().port);
-		});
-	}
+    function setupServer(server, type) {
+        server.on('error', function(err) {
+            (logHandler || console).log(err)
+        });
+        server.on('socketError', function(err) {
+            (logHandler || console).log(err)
+        });
+        server.on('request', ns);
+        server.on('listening', function() {
+            (logHandler || console).info(type + ' Server running: ', server.address().address + ':' + server.address().port);
+        });
+    }
 
-	function tcp() {
-		if (options.tcp) {
-			var tcpDns = ndns.createTCPServer();
-			setupServer(tcpDns, 'TCP');
-			tcpDns.serve(options.port, options.addr);
-		}
-	}
+    function tcp() {
+        if (options.tcp) {
+            var tcpDns = ndns.createTCPServer();
+            setupServer(tcpDns, 'TCP');
+            tcpDns.serve(options.port);
+        }
+    }
 
-	function udp() {
-		if (options.udp) {
-			var udpDns = ndns.createServer();
-			setupServer(udpDns, 'UDP');
-			udpDns.serve(options.portUdp, options.addr);
-		}
-	}
+    function udp() {
+        if (options.udp) {
+            var udpDns = ndns.createServer();
+            setupServer(udpDns, 'UDP');
+            udpDns.serve(options.portUdp);
+
+            if (options.ipv6) {
+                var udpDns = ndns.createServer({
+                    dgram_type : 'udp6'
+                });
+                setupServer(udpDns, 'UDP');
+                udpDns.serve(options.portUdp);
+            }
+
+        }
+    }
 
 });
 
 program.parse(process.argv);
 
 if (!program.args.length)
-	program.help();
+    program.help();
